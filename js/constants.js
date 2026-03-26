3db9ef8c00b251dbb07ee6d68ccbc21de7535766
ceevaa-ai <parth.b@ceevaa.ai>
2026-03-27 01:30:12 +0530

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 export const GRID_COLS = 22;
-export const GRID_ROWS = 8;
+export const GRID_ROWS = 5;
 export const SCRAMBLE_DURATION = 800;
 export const FLIP_DURATION = 300;
 export const STAGGER_DELAY = 25;
@@ -18,251 +18,176 @@ export const MESSAGES = [
   [
     '',
     'TO PLAY FOR MADRID',
-    '',
     'IS THE GREATEST',
-    '',
     'THING IN FOOTBALL',
-    '',
     '- DI STEFANO'
   ],
   [
     '',
     'REAL MADRID IS NOT',
-    '',
     'A CLUB. IT IS',
-    '',
     'A WAY OF LIFE',
-    '',
     '- ZIDANE'
   ],
   [
     '',
     'AT REAL MADRID',
-    '',
     'YOU HAVE TO WIN.',
-    '',
     'ALWAYS.',
-    '',
     '- FLORENTINO'
   ],
   [
     '',
     'THIS CLUB HAS A',
-    '',
     'MAGIC NO OTHER',
-    '',
     'CLUB IN THE WORLD',
-    '',
     '- RAUL'
   ],
   [
     '',
     'WINNING IS IN',
-    '',
-    'REAL MADRIDS',
-    '',
-    'DNA.',
+    'REAL MADRIDS DNA.',
     '',
     '- SERGIO RAMOS'
   ],
   [
     '',
     'THE BERNABEU',
-    '',
     'NEVER GIVES UP.',
-    '',
     'NEVER.',
-    '',
     '- CASILLAS'
   ],
   [
     '',
     'PLAYING FOR MADRID',
-    '',
     'MEANS PLAYING',
-    '',
     'FOR THE BEST',
-    '',
     '- R. NAZARIO'
   ],
   [
     '',
     'THIS SHIRT MAKES',
-    '',
     'ME FEEL SOMETHING',
-    '',
     'EXTRAORDINARY',
-    '',
     '- MARCELO'
   ],
   [
     '',
     'REAL MADRID',
-    '',
     'DOESN\'T REBUILD.',
-    '',
     'IT RELOADS.',
-    '',
     '- ANCELOTTI'
   ],
   [
     '',
     'THE WHITE SHIRT',
-    '',
     'CARRIES THE',
-    '',
     'WEIGHT OF HISTORY',
-    '',
     '- HIERRO'
   ],
   [
     '',
     'REAL MADRID',
-    '',
     'LIKE A FILM -',
-    '',
     'ALWAYS DRAMATIC',
-    '',
     '- MOURINHO'
   ],
   [
     '',
     'YOU NEVER COUNT',
-    '',
     'REAL MADRID OUT.',
-    '',
     'NEVER.',
-    '',
     '- GUARDIOLA'
   ],
   [
     '',
-    'A WINNING',
-    '',
-    'MENTALITY',
-    '',
-    'IMPOSSIBLE TO EXPLAIN',
-    '',
+    'A WINNING MENTALITY',
+    'IMPOSSIBLE TO',
+    'EXPLAIN',
     '- KLOPP'
   ],
   [
     '',
     'REAL MADRID',
-    '',
     'DON\'T DIE.',
-    '',
     'THEY JUST WAIT.',
-    '',
     '- UNKNOWN'
   ],
   [
     '',
     'THE GREATEST CLUB',
-    '',
     'IN THE HISTORY',
-    '',
     'OF FOOTBALL',
-    '',
     '- FIFA, 2000'
   ],
   [
     '',
     'CHAMPIONS AREN\'T',
-    '',
     'MADE IN GYMS.',
-    '',
-    '',
     '',
     '- MUHAMMAD ALI'
   ],
   [
     '',
     'NEVER GIVE UP.',
-    '',
     'THAT IS THE',
-    '',
     'SECRET OF SUCCESS',
-    '',
     '- ZIDANE'
   ],
   [
     '',
     'PRESSURE IS',
-    '',
     'A PRIVILEGE.',
-    '',
-    '',
     '',
     '- BILLIE JEAN KING'
   ],
   [
     '',
     'IT\'S NOT THE SIZE',
-    '',
     'OF THE DOG',
-    '',
     'IN THE FIGHT',
-    '',
     '- MARK TWAIN'
   ],
   [
     '',
     'WIN OR LOSE,',
-    '',
     'DO IT',
-    '',
     'WITH CLASS.',
-    '',
     '- UNKNOWN'
   ],
   [
     '',
     'HALA MADRID',
-    '',
     'Y NADA MAS.',
-    '',
-    '',
     '',
     '- CLUB ANTHEM'
   ],
   [
     '',
     'WE DON\'T NEED',
-    '',
     'LUCK. WE HAVE',
-    '',
     'HISTORY.',
-    '',
     '- UNKNOWN'
   ],
   [
     '',
     'IN MADRID,',
-    '',
     'MIRACLES ARE',
-    '',
     'EXPECTED.',
-    '',
     '- UNKNOWN'
   ],
   [
     '',
     'THE BERNABEU IS',
-    '',
     'THE CATHEDRAL',
-    '',
     'OF FOOTBALL',
-    '',
     '- PLATINI'
   ],
   [
     '',
     'TO WEAR WHITE',
-    '',
     'IS TO CARRY',
-    '',
     'A LEGACY.',
-    '',
     '- UNKNOWN'
   ]
 ];
